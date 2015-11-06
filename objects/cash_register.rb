c38ca47b57fbab73363c7a5644c93e49afd24834
Zach Alewel <zachalewel@gmail.com>
2015-11-06 10:16:58 -0700

    Changes to homework [#105810600]

--- objects/cash_register.rb
+++ objects/cash_register.rb
@@ -1,9 +1,12 @@
 # hey Rubocop
 class CashRegister
-  attr_reader :total
 
   def initialize
-    @total = 0.0
+    @total = 0
+  end
+
+  def total
+    format("%.2f", @total).to_f
   end
 
   def purchase(amount)
@@ -15,12 +18,30 @@ class CashRegister
     # @total = @total - amount
     @total -= amount
     # if the total is less than or = to zero then print
-    if total <= 0
+    if @total <= 0.0
       puts "your change is $#{-total}"
-      @total = 0
+      @total = 0.0
     else
       puts "your new balance is $#{total}"
     end
     total
   end
+
 end
+
+register = CashRegister.new
+puts register.total
+puts register.purchase(3.78)
+puts register.total
+puts register.pay(5.00)
+puts register.total
+
+register = CashRegister.new
+puts register.total
+puts register.purchase(3.78)
+puts register.purchase(5.22)
+puts register.total
+puts register.pay(5.00)
+puts register.total
+puts register.pay(5.00)
+puts register.total
